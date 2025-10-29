b4fb955c2e8f7f40f6d78eef1458d2d3d1f5140a
karthik <ponnurukarthik18@gmail.com>
2025-10-29 09:46:26 +0530

    chu

--- sapora_files/launch_client.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo   Sapora Video Conference Client
-echo ========================================
-echo.
-echo Starting PyQt6 GUI...
-echo.
-python client\main_ui.py
-pause
